0dc1cc73b0ab915cb4c95c401e34a9df43e534fe
Shriphani Palakodety <shriphanip@gmail.com>
2013-11-29 18:27:13 -0500

    required deps added

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clj-http "0.7.7"]
+                 [net.sourceforge.htmlcleaner/htmlcleaner "2.6"]
+                 [org.apache.commons/commons-lang3 "3.1"]])
