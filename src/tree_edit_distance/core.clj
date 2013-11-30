1b9db3769df743e1e1bed269045fc416af8a9e65
Shriphani Palakodety <shriphanip@gmail.com>
2013-11-30 01:15:58 -0500

    init using an array. better fit for dp than a map

--- src/tree_edit_distance/core.clj
+++ src/tree_edit_distance/core.clj
@@ -1,6 +1,24 @@
 (ns tree-edit-distance.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn init
+  [m n]
+  (let [M (make-array Integer/TYPE m n)]
+    (do
+      (doseq [i (range m)]
+        (aset M i 0 (int 0)))
+      (doseq [j (range n)]
+        (aset M 0 j (int 0)))
+      M)))
+
+(defn rtdm-edit-distance
+  "The RTDM algorithm for computing edit-distance.
+   The trees are assumed to be org.w3c.dom.Documents"
+  [tree-1 tree-2 thresh]
+
+  (let [tree1-children (.getChildNodes tree-1)
+        tree2-children (.getChildNodes tree-2)
+
+        m              (.getLength tree1-children)
+        n              (.getLength tree2-children)
+        M              (init m n)]
+    M))
