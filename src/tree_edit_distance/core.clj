0d4d6584e63e0a3f1aa2485a277f6114f695278a
Shriphani Palakodety <shriphanip@gmail.com>
2013-12-02 13:11:56 -0500

    prepare for artdm implementation

--- src/tree_edit_distance/core.clj
+++ src/tree_edit_distance/core.clj
@@ -2,6 +2,7 @@
   (:use [clojure.pprint]))
 
 (defn init
+  "Perform the correct initialization"
   [m n]
   (let [M (make-array Integer/TYPE (inc m) (inc n))]
     (do
@@ -28,66 +29,13 @@
 
 (defn tree-descendants
   [a-tree]
-  (cons a-tree
-        (if (.hasChildNodes a-tree)
-          (concat (tree-children a-tree)
-                  (flatten (map tree-descendants (tree-children a-tree))))
-          [])))
+  (if (.hasChildNodes a-tree)
+    (concat (tree-children a-tree)
+            (flatten (map tree-descendants (tree-children a-tree))))
+    ['*]))
 
 (defn rtdm-edit-distance
   "The RTDM algorithm for computing edit-distance.
    The trees are assumed to be org.w3c.dom.Documents"
   [tree-1 tree-2 thresh del-cost ins-cost sub-cost]
-  (println "Tree1: " (.getNodeName tree-1))
-  (println "Tree2: " (.getNodeName tree-2))
-  (let [m (num-children tree-1)
-        n (num-children tree-2)
-        M (init m n)]
-    (do
-     (doseq [i (range m)
-             j (range n)]
-       
-       (let [c1  (tree-children tree-1)
-             c2  (tree-children tree-2)
-             
-             c-i (tree-descendants (nth c1 i))
-             c-j (tree-descendants (nth c2 j))
-
-             _   (println "Tree1-desc:" (count c-i) (.getNodeName (nth c1 i)))
-             _   (println "Tree2-desc:" (count c-j) (.getNodeName (nth c2 j)))
-             
-             del (+ (aget M i (inc j))
-                    (apply + (map del-cost c-i)))
-             ins (+ (aget M (inc i) j)
-                    (apply + (map ins-cost c-j)))
-
-             s   (cond (> (aget M i j)
-                          thresh)
-                       Double/POSITIVE_INFINITY
-                       
-                       (.isEqualNode (nth c1 i)
-                                     (nth c2 j))
-                       0
-                       
-                       :else
-                       (cond (not (.hasChildNodes (nth c1 i)))
-                             (+ (sub-cost (nth c1 i) (nth c2 j))
-                                (apply + (map ins-cost c-j)))
-
-                             (not (.hasChildNodes (nth c2 j)))
-                             (+ (sub-cost (nth c1 i) (nth c2 j))
-                                (apply + (map del-cost c-i)))
-
-                             :else
-                             (rtdm-edit-distance (nth c1 i)
-                                                 (nth c2 j)
-                                                 thresh
-                                                 del-cost
-                                                 ins-cost
-                                                 sub-cost)))]
-         (println ins del s)
-         (pprint M)
-         (println :i (inc i) :j (inc j))
-         (aset M (inc i) (inc j) (int (min del ins s)))
-         (pprint M)))
-     (aget M m n))))
+  '*)
